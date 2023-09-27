4b6dffa1b56b3f03fb783e72a39fdde700f19939
iamprincetj <tochukwunwanze5@gmail.com>
2023-09-27 17:13:43 +0100

    7-load_balancer.js done

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  const func1 = await uploadPhoto();
+  const func2 = await createUser();
+
+  return {
+    photo: func1,
+    user: func2,
+  };
+}
+
+console.log(asyncUploadUser().then((res) => console.log(res)));
