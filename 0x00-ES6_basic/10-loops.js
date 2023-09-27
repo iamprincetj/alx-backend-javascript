7e7e914684ff09be86fc675746295b0296ce855a
iamprincetj <tochukwunwanze5@gmail.com>
2023-09-28 00:36:15 +0100

    10-loops.js done

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  for (const idx of array) {
+    array[array.indexOf(idx)] = appendString + idx; // eslint-disable-line no-param-reassign
+  }
+
+  return array;
+}
+console.log(appendToEachArrayValue(['appended', 'fixed', 'displayed'], 'correctly-'));
