4ffd75918e65623558c63dcc60fcee23ad6358f0
iamprincetj <tochukwunwanze5@gmail.com>
2023-09-28 17:39:53 +0100

    4-pricing.js done

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-underscore-dangle */
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(val) {
+    this._amount = val;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(val) {
+    this._currency = val;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
+const p = new Pricing(100, new Currency('EUR', 'Euro'));
+console.log(p);
+console.log(p.displayFullPrice());
