459a96f798791052fbb49966f85725b15d24dc70
iamprincetj <tochukwunwanze5@gmail.com>
2023-09-28 19:18:07 +0100

    100-evcar.js done

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -12,15 +12,3 @@ export default class Car {
     return new this.constructor();
   }
 }
-class TestCar extends Car {}
-
-const tc1 = new TestCar('Nissan', 'Turbo', 'Pink');
-const tc2 = tc1.cloneCar();
-
-console.log(tc1);
-console.log(tc1 instanceof TestCar);
-
-console.log(tc2);
-console.log(tc2 instanceof TestCar);
-
-console.log(tc1 === tc2);
