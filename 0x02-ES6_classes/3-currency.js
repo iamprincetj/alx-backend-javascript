1df32040e0116f1a2f608db823d11aad0150ca1a
iamprincetj <tochukwunwanze5@gmail.com>
2023-09-28 17:38:58 +0100

    3-currency.js done

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-underscore-dangle */
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(val) {
+    this._code = val;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(val) {
+    this._name = val;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
