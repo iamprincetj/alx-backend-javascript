d14f663b083081210e82c6e37e849e11b21f5a3b
iamprincetj <tochukwunwanze5@gmail.com>
2023-10-03 12:02:22 +0100

    10-update_uniq_items.js done

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,7 @@
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) throw Error('Cannot process');
+  map.forEach((val, key) => {
+    if (val === 1) map.set(key, 100);
+  });
+  return map;
+}
